0985afd80b86e8f429471f57c7e0f73c3bf981a4
Nicholas Crofts <nicholas.crofts@gmail.com>
2019-10-25 13:46:34 -0400

    lesson 22

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -7,18 +7,31 @@ class App extends Component {
     super()
 
     this.state = {
-      string: 'Cool Cool'
+      monsters: [
+        {
+          name: 'Frankenstein',
+          id: '1'
+        },
+        {
+          name: 'Dracula',
+          id: '2'
+        },
+        {
+          name: 'Zombie',
+          id:'3'
+        }
+      ]
     }
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{this.state.string}</p>
-          <button onClick={() => this.setState({ string: 'Tight Tight' })}>Change Text</button>
-        </header>
+        {
+          this.state.monsters.map(monster => (
+            <h1 key={ monster.id }> { monster.name } </h1>
+          ))
+        }
       </div>
     )
   }
