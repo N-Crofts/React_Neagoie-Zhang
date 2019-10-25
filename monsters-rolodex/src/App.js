a179d1378f82f938928cbd5100276815dcc406a6
Nicholas Crofts <nicholas.crofts@gmail.com>
2019-10-25 10:50:00 -0400

    goofing around but still committin

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Crunk 'till you die.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.nicholascrofts.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Gimme Money
         </a>
       </header>
     </div>
